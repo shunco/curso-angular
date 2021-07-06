09224107e8b7332059c904bcba8bab46f09b44d8
Cesar Castillo <ccastillo@datafierce.com>
2021-07-05 22:47:51 -0500

    05/07/2021

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Menu } from './models/menu';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'tecgurus';
+  isOpen: boolean = true;
+
+  public menus: Menu[] = [{
+    title: 'Modulos',
+    key: 'modulos',
+    children: [
+      {
+        title: 'Componente de Modulos Externos',
+        path: 'componente_externos'
+      }, {
+        title: 'Componentes Internos',
+        path: 'componente_internos'
+      },{
+        title: 'Componentes Externos Web',
+        path: 'componentes_externo_web'
+      },{
+        title: 'Modulo Carga Lenta',
+        path: 'module_carga_lenta'
+      }
+    ]
+
+  },{
+    title: 'Usuario',
+    path: 'usuarios'
+  }]
+
 }
---
+++ src/app/models/menu.ts
@@ -0,0 +1,6 @@
+export interface Menu {
+    title: string;
+    key?: string;
+    children?: Menu[];
+    path?: string;
+}
