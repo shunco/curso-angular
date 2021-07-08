01baaa1beae7fe3eca9914cd3f5f5f8e2b96b990
Cesar Castillo <ccastillo@datafierce.com>
2021-07-07 20:25:49 -0500

    añadiendo una lista de usuarios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,22 @@ import { SharedModule } from './modules/shared/shared.module';
 import { ExternalComponentComponent } from './pages/external-component/external-component.component';
 import { DataTablesModule } from 'angular-datatables';
 import { WebComponentComponent } from './pages/web-component/web-component.component';
-import { InternalComponentsComponent } from './pages/internal-components/internal-components.component'; 
+import { InternalComponentsComponent } from './pages/internal-components/internal-components.component';
+import { WebComponent } from './pages/internal-components/web/web.component';
+import { ManagerComponent } from './pages/internal-components/manager/manager.component';
+import { AdminComponent } from './pages/internal-components/admin/admin.component';
+import { ArtComponent } from './pages/internal-components/art/art.component'; 
 
 @NgModule({
   declarations: [
     AppComponent,
     ExternalComponentComponent,
     WebComponentComponent,
-    InternalComponentsComponent
+    InternalComponentsComponent,
+    WebComponent,
+    ManagerComponent,
+    AdminComponent,
+    ArtComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/models/usuario.ts
@@ -0,0 +1,6 @@
+export interface Usuario {
+    name: string;
+    type: string;
+}
+
+
