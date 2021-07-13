9235c02c45f80fdf02c71191d6de7dd0ed253859
Cesar Castillo <ccastillo@datafierce.com>
2021-07-12 20:28:58 -0500

    un repaso general de observables

--- src/app/modules/observable/observable.component.ts
+++ src/app/modules/observable/observable.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable , Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-observable',
@@ -7,9 +8,144 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ObservableComponent implements OnInit {
 
-  constructor() { }
+
+
+  sydney$: Observable<number>;
+  sydney: number;
+
+
+  melbourne$: Observable<number>;
+
+
+  lithgow$: Observable<number>;
+  lithgow: number;
+
+  darvin$: Observable<number>;
+
+
+  mexico_city$: Observable<number>;
+  mexico_city: number;
+  subscription: Subscription;
+
+
+
+  monterrey$: Observable<number>;
+  monterrey: number;
+
+  constructor() {
+
+  }
 
   ngOnInit() {
+
+    this.sydney$ = this.getObservable();
+    this.sydney$.subscribe(weather => {
+      this.sydney = weather;
+    })
+
+
+    this.melbourne$ = this.getObservable();
+
+
+    this.lithgow$ = this.getObservableTime();
+    this.lithgow$.subscribe(weather => {
+      this.lithgow = weather;
+    });
+
+    this.darvin$ = this.getObservableTime();
+
+
+
+    this.mexico_city$ = this.getObservableTime();
+    this.subscription = this.mexico_city$.subscribe(weather => {
+      this.mexico_city = weather;
+    });
+
+    setTimeout(()=> {
+      this.subscription.unsubscribe();
+    }, 5000);
+
+
+    this.monterrey$ = this.getObservableError();
+    this.monterrey$.subscribe(weather => {
+      this.monterrey = weather;
+    }, error=> {
+      console.log(error)
+    }, ()=> {
+      console.log("termine");
+    });
+
+
+
+
+
   }
+
+
+  public getObservable(): Observable<number> {
+
+    return Observable.create(observer => {
+      observer.next(this.getRandom());
+      observer.complete();
+    })
+
+  }
+
+  public getObservableTime(): Observable<number> {
+
+    return Observable.create(observer => {
+
+      observer.next(this.getRandom());
+
+      setTimeout(() => {
+        observer.next(this.getRandom());
+      }, 4000);
+
+      setTimeout(() => {
+        observer.next(this.getRandom());
+      }, 6000);
+
+
+      setTimeout(() => {
+        observer.next(this.getRandom());
+        observer.complete();
+      }, 8000);
+
+    })
+
+  }
+
+
+  public getObservableError(): Observable<number> {
+
+    return Observable.create(observer => {
+
+      observer.next(this.getRandom());
+
+      setTimeout(() => {
+        observer.next(this.getRandom());
+      }, 4000);
+
+      setTimeout(() => {
+        observer.error('Hubo un error');
+      }, 6000);
+
+
+      setTimeout(() => {
+        observer.next(this.getRandom());
+        observer.complete();
+      }, 8000);
+
+    })
+
+  }
+
+
+  public getRandom(): number {
+    return Math.floor(Math.random() * (40 - 5)) + 5;
+  }
+
+
+
 
 }
